e6f7e91f1714adc3aef1d852d93f85badad64f12
Christian David <c.david@videotovoice.com>
2017-04-02 17:43:17 +0200

    Enhanced & refactored FT

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,7 @@
-# Create your views here.
 from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'lists/home.html')
+    return render(request, 'lists/home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+    })
